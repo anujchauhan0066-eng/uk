1a73f403779652162f4f6dc9cf77cfafb80b5b62
anujchauhan0066-eng <anujchauhan0066@gmail.com>
2026-08-08 08:45:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,14 +18,14 @@ const getBaseUrl = (req) => {
 const AD_SCRIPT = `
 <script>
   atOptions = {
-    'key' : '72b6f3ac3fc2f43722e5f2196ef85add',
+    'key' : 'aeb6543c6570e1a94e3b020737bb04b0',
     'format' : 'iframe',
     'height' : 90,
     'width' : 728,
     'params' : {}
   };
 </script>
-<script src="https://www.highperformanceformat.com/72b6f3ac3fc2f43722e5f2196ef85add/invoke.js"></script>
+<script src="https://industriousslowly.com/aeb6543c6570e1a94e3b020737bb04b0/invoke.js"></script>
 `;
 
 const AD_TOP = `
@@ -170,14 +170,14 @@ ${AD_TOP}
     <div>
 <script>
   atOptions = {
-    'key' : 'd1b072857c7132ec474a48b3413701e2',
+    'key' : 'f249b6ec1e626effe1ac813e124ca110',
     'format' : 'iframe',
     'height' : 60,
     'width' : 468,
     'params' : {}
   };
 </script>
-<script src="https://www.highperformanceformat.com/d1b072857c7132ec474a48b3413701e2/invoke.js"></script>
+<script src="https://industriousslowly.com/f249b6ec1e626effe1ac813e124ca110/invoke.js"></script>
 </div>
 </div>
 ${bodyContent}
